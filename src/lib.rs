bf86c676fd1cf76854157e4b807ad07229f3b2ae
Kornel <kornel@geekhood.net>
2019-01-20 18:14:21 +0000

    Optimizations

--- src/lib.rs
+++ src/lib.rs
@@ -22,7 +22,6 @@
 //! }
 //! ```
 
-use std::collections::hash_map::Entry;
 use std::char;
 use std::cmp;
 use std::f32;
@@ -64,43 +63,31 @@ impl Canvas {
     /// Sets a pixel at the specified coordinates.
     pub fn set(&mut self, x: u32, y: u32) {
         let (row, col) = ((x / 2) as u16, (y / 4) as u16);
-        match self.chars.entry((row, col)) {
-            Entry::Occupied(_) => {},
-            Entry::Vacant(e) => { e.insert(0); },
-        }
-        self.chars.get_mut(&(row, col)).map(|a| *a |= PIXEL_MAP[y as usize % 4][x as usize % 2]);
+        let a = self.chars.entry((row, col)).or_insert(0);
+        *a |= PIXEL_MAP[y as usize % 4][x as usize % 2];
     }
 
     /// Deletes a pixel at the specified coordinates.
     pub fn unset(&mut self, x: u32, y: u32) {
         let (row, col) = ((x / 2) as u16, (y / 4) as u16);
-        match self.chars.entry((row, col)) {
-            Entry::Occupied(_) => {},
-            Entry::Vacant(e) => { e.insert(0); },
-        }
-        self.chars.get_mut(&(row, col)).map(|a| *a &= !PIXEL_MAP[y as usize % 4][x as usize % 2]);
+        let a = self.chars.entry((row, col)).or_insert(0);
+        *a &= !PIXEL_MAP[y as usize % 4][x as usize % 2];
     }
 
     /// Toggles a pixel at the specified coordinates.
     pub fn toggle(&mut self, x: u32, y: u32) {
         let (row, col) = ((x / 2) as u16, (y / 4) as u16);
-        match self.chars.entry((row, col)) {
-            Entry::Occupied(_) => {},
-            Entry::Vacant(e) => { e.insert(0); },
-        }
-        self.chars.get_mut(&(row, col)).map(|a| *a ^= PIXEL_MAP[y as usize % 4][x as usize % 2]);
+        let a = self.chars.entry((row, col)).or_insert(0);
+        *a ^= PIXEL_MAP[y as usize % 4][x as usize % 2];
     }
 
     /// Detects whether the pixel at the given coordinates is set.
     pub fn get(&self, x: u32, y: u32) -> bool {
-        let dot_index = PIXEL_MAP[y as usize % 4][x as usize % 2];
         let (row, col) = ((x / 2) as u16, (y / 4) as u16);
-        let char = self.chars.get(&(row, col));
-
-        match char {
-            None => false,
-            Some(c) => *c & dot_index != 0,
-        }
+        self.chars.get(&(row, col)).map_or(false, |c| {
+            let dot_index = PIXEL_MAP[y as usize % 4][x as usize % 2];
+            *c & dot_index != 0
+        })
     }
 
     /// Returns a `Vec` of each row of the `Canvas`.
@@ -108,13 +95,17 @@ impl Canvas {
     /// Note that each row is actually four pixels high due to the fact that a single Braille
     /// character spans two by four pixels.
     pub fn rows(&self) -> Vec<String> {
-        let maxrow = cmp::max(self.width, self.chars.keys().map(|&(x, _)| x).max().unwrap_or(0));
-        let maxcol = cmp::max(self.height, self.chars.keys().map(|&(_, y)| y).max().unwrap_or(0));
+        let mut maxrow = self.width;
+        let mut maxcol = self.height;
+        for &(x, y) in self.chars.keys() {
+            if x > maxrow {maxrow = x;}
+            if y > maxcol {maxcol = y;}
+        }
 
-        let mut result = vec![];
-        for y in 0..maxcol + 1 {
-            let mut row = String::new();
-            for x in 0..maxrow + 1 {
+        let mut result = Vec::with_capacity(maxcol as usize + 1);
+        for y in 0..=maxcol {
+            let mut row = String::with_capacity(maxrow as usize + 1);
+            for x in 0..=maxrow {
                 let char = self.chars.get(&(x, y)).cloned().unwrap_or(0) as u32;
                 row.push(if char == 0 {
                     ' '
@@ -129,10 +120,11 @@ impl Canvas {
 
     /// Draws the canvas to a `String` and returns it.
     pub fn frame(&self) -> String {
-        self.rows().into_iter().collect::<Vec<String>>().join("\n")
+        self.rows().join("\n")
     }
 
-    fn line_vec(&self, x1: u32, y1: u32, x2: u32, y2: u32) -> Vec<(u32, u32)> {
+    /// Draws a line from `(x1, y1)` to `(x2, y2)` onto the `Canvas`.
+    pub fn line(&mut self, x1: u32, y1: u32, x2: u32, y2: u32) {
         let xdiff = cmp::max(x1, x2) - cmp::min(x1, x2);
         let ydiff = cmp::max(y1, y2) - cmp::min(y1, y2);
         let xdir = if x1 <= x2 { 1 } else { -1 };
@@ -140,8 +132,7 @@ impl Canvas {
 
         let r = cmp::max(xdiff, ydiff);
 
-        let mut result = vec![];
-        for i in 0..r + 1 {
+        for i in 0..=r {
             let mut x = x1 as i32;
             let mut y = y1 as i32;
 
@@ -152,15 +143,7 @@ impl Canvas {
                 x += ((i * xdiff) / r) as i32 * xdir;
             }
 
-            result.push((x as u32, y as u32));
-        }
-        result
-    }
-
-    /// Draws a line from `(x1, y1)` to `(x2, y2)` onto the `Canvas`.
-    pub fn line(&mut self, x1: u32, y1: u32, x2: u32, y2: u32) {
-        for &(x, y) in self.line_vec(x1, y1, x2, y2).iter() {
-            self.set(x, y);
+            self.set(x as u32, y as u32);
         }
     }
 }
